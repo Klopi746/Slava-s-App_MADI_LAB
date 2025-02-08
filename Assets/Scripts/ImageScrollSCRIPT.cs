d30e7cd74ec6239140d946268a070680ad04cd68
Steel Bocharov <132206705+Klopi746@users.noreply.github.com>
2025-02-08 14:42:19 +0300

    0.05 - scene1 ready

--- Assets/Scripts/ImageScrollSCRIPT.cs
+++ Assets/Scripts/ImageScrollSCRIPT.cs
@@ -4,9 +4,28 @@ using UnityEngine.EventSystems;
 public class ImageScrollSCRIPT : MonoBehaviour, IScrollHandler
 {
     [SerializeField] int rotateScale = 1;
+    [SerializeField] int minAnchoredPosY = -200;
+    [SerializeField] int maxAnchoredPosY = 200;
+
+    private RectTransform rectTransform;
+
+    private void Awake()
+    {
+        rectTransform = transform.GetComponent<RectTransform>();
+    }
     public void OnScroll(PointerEventData eventData)
     {
-        Vector3 rotationVector = new Vector3(0, eventData.scrollDelta.y, 0) * rotateScale;
-        transform.Translate(rotationVector, Space.Self);
+        if (eventData.scrollDelta.y < 0)
+        {
+            if (rectTransform.anchoredPosition.y > maxAnchoredPosY) return;
+            Vector3 rotationVector = new Vector3(0, -eventData.scrollDelta.y, 0) * rotateScale;
+            transform.Translate(rotationVector, Space.Self);
+        }
+        else
+        {
+            if (rectTransform.anchoredPosition.y < minAnchoredPosY) return;
+            Vector3 rotationVector = new Vector3(0, -eventData.scrollDelta.y, 0) * rotateScale;
+            transform.Translate(rotationVector, Space.Self);
+        }
     }
 }
