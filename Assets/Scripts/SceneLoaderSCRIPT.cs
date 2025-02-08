57b2f6b643ae92b0fe94ebda4c4a95b6168aeb1f
Steel Bocharov <132206705+Klopi746@users.noreply.github.com>
2025-02-08 15:15:42 +0300

    0.06 - scene2 readyness

--- Assets/Scripts/SceneLoaderSCRIPT.cs
+++ Assets/Scripts/SceneLoaderSCRIPT.cs
@@ -1,6 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoaderSCRIPT : MonoBehaviour
 {
     public static SceneLoaderSCRIPT Instance;
+
+    public void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
 }
